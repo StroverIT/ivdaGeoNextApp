1f923ff79be91fbd7c70106bea3492550ca78d22
StR0vErBG <xcreaw1@abv.bg>
2022-05-06 17:54:17 +0200

    Creating layout

---
+++ components/layouts/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
+export default function Layout({children}) {
+  return (
+    <>
+    <Navbar />
+    <main>
+        {children}
+    </main>
+    <Footer />
+    </>
+  )
+}
