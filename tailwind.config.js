8208a15b1c55d10a02e8fdf86ab14e94e6d2f795
StR0vErBG <xcreaw1@abv.bg>
2022-05-06 15:11:40 +0200

    adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,26 @@ module.exports = {
       '2xl': {'min': '1536px'},
       // => @media (min-width: 1536px) { ... }
     },
+    colors: {
+      'bg-color': 'hsl(200, 35%, 92%)',
+      'primary-color': 'hsl(215, 80%, 33%)',
+      'secondary-color': 'hsl(358, 83%, 56%)',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {},
   },
   plugins: [],
 }
+
+// $primary-color: hsl(215, 80%, 33%);
+// $sec-color: hsl(358, 83%, 56%);
+// $background-color: hsl(200, 35%, 92%);
+// $white-color: hsl(210, 30%, 92%);
+// $blue-color:hsl(203, 76%, 48%);
+// $red-color: hsl(344, 92%, 58%);
+// $orange-color: hsl(33, 89%, 52%);
+// $green-color: hsl(92, 50%, 54%);
+// $gray-color: hsl(0, 0%, 96%);
