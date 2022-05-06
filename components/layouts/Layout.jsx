93e1f91fc578a3a62be2e24f256ce89a73e3abb1
StR0vErBG <xcreaw1@abv.bg>
2022-05-06 18:10:37 +0200

    Working layout

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -4,12 +4,10 @@ import Navbar from "./Navbar"
 
 export default function Layout({children}) {
   return (
-    <>
+    <div className="flex flex-col h-screen justify-between">
     <Navbar />
-    <main>
         {children}
-    </main>
     <Footer />
-    </>
+    </div>
   )
 }
