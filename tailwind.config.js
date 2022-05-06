bb6b7575453233f699c9d5383110885212cb29ac
StR0vErBG <xcreaw1@abv.bg>
2022-05-06 14:08:41 +0200

    Adding media querieis

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': {'min': '640px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+  
+      'md': {'min': '768px'},
+      // => @media (min-width: 768px ) { ... }
+  
+      'lg': {'min': '1024px'},
+      // => @media (min-width: 1024px ) { ... }
+  
+      'xl': {'min': '1280px'},
+      // => @media (min-width: 1280px) { ... }
+  
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
