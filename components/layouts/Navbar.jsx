16ef719d8959a8a035d34ffdc536c7a34dacf6aa
StR0vErBG <xcreaw1@abv.bg>
2022-05-06 18:10:47 +0200

    Testing links

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Link from "next/link"
 
 const Navbar = () => {
+
     return (
         <nav>
-            
+            <ul>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            </ul>
+           
         </nav>
     );
 }
