edaa171bc18d71d7a43757c61898d4a4a43d695e
StR0vErBG <xcreaw1@abv.bg>
2022-05-07 19:13:44 +0200

    Addin # to href

--- components/layouts/navComponents/navLinks.jsx
+++ components/layouts/navComponents/navLinks.jsx
@@ -1,48 +1,50 @@
+// React and nextJs things
 import React from 'react';
 
+// Styles
 const NavLinks = () => {
     return (
-        <ul class="navigation">
-    <li class="menu">Интрументи и железария</li>
-    <ul class="submenu">
-        <li><a href="">Акумулаторни комплекти</a></li>
-        <li><a href="">Тест</a></li>
-    </ul>
+        <ul className="navigation">
+            <li className="menu">Интрументи и железария</li>
+                <ul className="submenu">
+                    <li><a href="#">Акумулаторни комплекти</a></li>
+                    <li><a href="#">Тест</a></li>
+                </ul>
 
-    <li class="menu">Градина</li>
-    <ul class="submenu">
-        <li>
-            <a href="">Градински мебели</a>
-        </li>
-    </ul>
+            <li className="menu">Градина</li>
+            <ul className="submenu">
+                <li>
+                    <a href="#">Градински мебели</a>
+                </li>
+            </ul>
 
-    <li class="menu">Кухня</li>
-    <ul class="submenu">
-        <li>
-            <a href="">Кухненски мебели</a>
-        </li>
-    </ul>
+            <li className="menu">Кухня</li>
+            <ul className="submenu">
+                <li>
+                    <a href="#">Кухненски мебели</a>
+                </li>
+            </ul>
 
-    <li class="menu">Баня</li>
-    <ul class="submenu">
-        <li>
-            <a href="">Мебели и огледала за баня</a>
-        </li>
-    </ul>
+        <li className="menu">Баня</li>
+        <ul className="submenu">
+            <li>
+                <a href="#">Мебели и огледала за баня</a>
+            </li>
+        </ul>
 
-    <li class="menu">Отопление, Охлаждане и Вик</li>
-    <ul class="submenu">
-        <li>
-            <a href="">Климатици</a>
-        </li>
-    </ul>
+        <li className="menu">Отопление, Охлаждане и Вик</li>
+        <ul className="submenu">
+            <li>
+                <a href="#">Климатици</a>
+            </li>
+        </ul>
 
-    <li class="menu">Подови и стенни покрития</li>
-    <ul class="submenu">
-        <li>
-            <a href="">Плочки и лайсни</a>
-        </li>
-    </ul>
+        <li className="menu">Подови и стенни покрития</li>
+        <ul className="submenu">
+            <li>
+                <a href="#">Плочки и лайсни</a>
+            </li>
+        </ul>
 
 
 </ul>
