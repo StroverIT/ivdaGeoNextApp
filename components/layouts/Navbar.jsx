e8133b4c513d3951a4c594e3b12a5151e079dfba
StR0vErBG <xcreaw1@abv.bg>
2022-05-07 20:53:53 +0200

    Adding ref to head for height

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import Link from "next/link"
 
 // Nav Components
 import Hamburger from "./navComponents/Hamburger"
 
 const Navbar = () => {
-  
-    
-
+    const headerRef = useRef(null)
     return (
-        <nav className="bg-primary py-3 relative z-50">
-            <div className="container flex flex-center items-center">
-            <Hamburger />
+        <header className="bg-primary z-50" ref={headerRef}>
+
+        <nav className="container flex flex-center items-center relative bg-primary py-3">
+            <Hamburger headRef={headerRef}/>
                 <ul >
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
                 </ul>
-            </div>
            
         </nav>
+        </header>
+
     );
 }
 
