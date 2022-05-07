cf37b0e804128fe936da74f97e54c48577b95cc8
StR0vErBG <xcreaw1@abv.bg>
2022-05-07 18:07:17 +0200

    Adding hamburger and fixing spacing

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Link from "next/link"
 
+// Nav Components
+import Hamburger from "./navComponents/Hamburger"
+
 const Navbar = () => {
+  
 
     return (
-        <nav className="bg-primary">
-            <div className="container">
-
-            <ul>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            </ul>
+        <nav className="bg-primary py-3">
+            <div className="container flex flex-center items-center">
+            <Hamburger />
+                <ul>
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                </ul>
             </div>
            
         </nav>
