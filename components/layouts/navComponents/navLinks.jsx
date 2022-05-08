f9251bae182cfd748a902eb685afb1f025a75985
StR0vErBG <xcreaw1@abv.bg>
2022-05-08 21:21:41 +0200

    Creating dictionary and gettig navLinkMenu compone

--- components/layouts/navComponents/navLinks.jsx
+++ components/layouts/navComponents/navLinks.jsx
@@ -1,56 +1,38 @@
 // React and nextJs things
-import React,{useRef} from 'react';
+import React from 'react';
 import Link from "next/link"
 
 // Styles
+// Components
+import NavLinkMenu from "./NavLinkMenu"
+
+const dictionary = [
+     {
+        title: "Интрументи и железария",
+        articles: [
+            "Акумулаторни комплекти",
+            "Test"
+        ],
+        icon: "",
+    },
+    {
+        title: "Градина",
+        articles: [
+            "Градински мебели",
+        ],
+        icon: "",
+    }
+]
 const NavLinks = () => {
-    const navigation = useRef(null)
-    
+
+
     return (
-        <ul className="navigation" ref={navigation}>
-            <li className="menu">Интрументи и железария</li>
-                <ul className="submenu hidden">
-                    <li><a href="">Акумулаторни комплекти</a></li>
-                    <li><a href="">Тест</a></li>
-                </ul>
-
-            <li className="menu">Градина</li>
-            <ul className="submenu hidden">
-                <li>
-                    <a href="">Градински мебели</a>
-                </li>
-            </ul>
-
-            <li className="menu">Кухня</li>
-            <ul className="submenu hidden">
-                <li>
-                    <a href="">Кухненски мебели</a>
-                </li>
-            </ul>
-
-        <li className="menu">Баня</li>
-        <ul className="submenu hidden">
-            <li>
-                <a href="">Мебели и огледала за баня</a>
-            </li>
-        </ul>
-
-        <li className="menu">Отопление, Охлаждане и Вик</li>
-        <ul className="submenu hidden">
-            <li>
-                <a href="">Климатици</a>
-            </li>
-        </ul>
-
-        <li className="menu">Подови и стенни покрития</li>
-        <ul className="submenu hidden">
-            <li>
-                <Link  href="/"><a href="">Плочки и лайсни</a></Link>
-            </li>
-        </ul>
-
-
+        <ul className="navigation" >
+            {dictionary.map(article=>{
+            return <NavLinkMenu title={article.title} articles={article.articles} key={article.title}/>
+            })}
 </ul>
+
     );
 }
 
