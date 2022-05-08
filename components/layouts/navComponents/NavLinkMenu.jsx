b2abc0451095ba4c9699abe7011b16fd4975b07d
StR0vErBG <xcreaw1@abv.bg>
2022-05-08 21:55:03 +0200

    Adding main route

--- components/layouts/navComponents/NavLinkMenu.jsx
+++ components/layouts/navComponents/NavLinkMenu.jsx
@@ -4,16 +4,17 @@ import Link from "next/link"
 // Styles
 import style from "../../../styles/navigation/NavLinks.module.css"
 
-const NavLinkMenu = ({title, articles}) => {
+const NavLinkMenu = ({title, articles,mainRoute}) => {
     return (
         <>
     <li className={` ${style.menu} ${style.menu}`}>{title}</li>
                 <ul className={`${style.submenu} hidden`}>
                     {articles.map(article=>{
+                        console.log(`/${mainRoute}/.${article[1]}`);
                         return (
-                        <Link href="/"> 
-                        <li key={article}>
-                            <a href="">{article}</a>
+                        <Link href={`/${mainRoute}/Add${article[1]}`} key={article[0]}> 
+                        <li>
+                            <a href="#">{article[0]}</a>
                             </li>
                             </Link>
                         )
