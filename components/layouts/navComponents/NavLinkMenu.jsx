f1412891d470d249bd4c766ce19068b758b90c4d
StR0vErBG <xcreaw1@abv.bg>
2022-05-08 22:07:23 +0200

    Working but not optimized

--- components/layouts/navComponents/NavLinkMenu.jsx
+++ components/layouts/navComponents/NavLinkMenu.jsx
@@ -5,13 +5,17 @@ import Link from "next/link"
 import style from "../../../styles/navigation/NavLinks.module.css"
 
 const NavLinkMenu = ({title, articles,mainRoute}) => {
+    function showMenu(e){
+        const siblingEl = e.target.nextElementSibling
+        siblingEl.classList.remove("hidden")
+    }
     return (
         <>
-    <li className={` ${style.menu} ${style.menu}`}>{title}</li>
+    <li className={` ${style.menu} ${style.menu}`} onClick={showMenu}>{title}</li>
                 <ul className={`${style.submenu} hidden`}>
                     {articles.map(article=>{
-                        console.log(`/${mainRoute}/.${article[1]}`);
                         return (
+
                         <Link href={`/${mainRoute}/Add${article[1]}`} key={article[0]}> 
                         <li>
                             <a href="#">{article[0]}</a>
