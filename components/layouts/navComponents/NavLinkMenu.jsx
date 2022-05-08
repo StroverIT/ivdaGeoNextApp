1a1bbd2a967381633d197136e721f1e965fa821e
StR0vErBG <xcreaw1@abv.bg>
2022-05-08 22:31:25 +0200

    Adding tailwind breakpoints

--- components/layouts/navComponents/NavLinkMenu.jsx
+++ components/layouts/navComponents/NavLinkMenu.jsx
@@ -3,17 +3,29 @@ import Link from "next/link"
 
 // Styles
 import style from "../../../styles/navigation/NavLinks.module.css"
+// Tailwind conf
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../../tailwind.config.js'
 
+const fullConfig = resolveConfig(tailwindConfig)
+const lg = fullConfig.theme.screens.lg.min.split("px")[0]
 const NavLinkMenu = ({title, articles,mainRoute}) => {
+    
     function showMenu(e){
+        if(window.innerWidth >= lg) return
+
         const siblingEl = e.target.nextElementSibling
         siblingEl.classList.remove("hidden")
+        siblingEl.style.left= `0px`
+
     }
     function addSpacing(e){
-        const parentEl = e.target.parentElement
-       parentEl.offsetWidth
-        const siblingEl = e.target.nextElementSibling
-        siblingEl.style.left= `${parentEl.offsetWidth}px`
+        if(window.innerWidth < lg) return
+      
+            const parentEl = e.target.parentElement
+            parentEl.offsetWidth
+             const siblingEl = e.target.nextElementSibling
+             siblingEl.style.left= `${parentEl.offsetWidth}px`
     }
     return (
         <>
