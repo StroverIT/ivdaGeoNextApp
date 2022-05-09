6c220f37b15437888c48740cb3195e471559e98d
StR0vErBG <xcreaw1@abv.bg>
2022-05-09 13:52:20 +0200

    FIxing bugs

--- components/layouts/navComponents/NavLinkMenu.jsx
+++ components/layouts/navComponents/NavLinkMenu.jsx
@@ -1,5 +1,7 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import Link from "next/link"
+import { useRouter } from 'next/router'
+import classNames from "classnames"
 
 // Styles
 import style from "../../../styles/navigation/NavLinks.module.css"
@@ -11,46 +13,57 @@ const fullConfig = resolveConfig(tailwindConfig)
 const lg = fullConfig.theme.screens.lg.min.split("px")[0]
 
 const NavLinkMenu = ({title, articles,mainRoute}) => {
-    
+    const router = useRouter()
+
+    const [hidden, setHidden] = useState(true)
+    const [left, setLeft] = useState(`0px`)
     const menu = useRef(null)
     const subMenu = useRef(null)
-    React.useEffect(() => {
+
+    useEffect(() => {
         function handleResize() {
-        //   console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
         if(window.innerWidth >= lg){
-            subMenu.current.classList.add("hidden")
-            subMenu.current.style.left= `${menu.offsetWidth}px`
+
+            setHidden(true)
+            setLeft( `${menu.offsetWidth}px`)
+           
         }
     }
-    
         window.addEventListener('resize', handleResize)
-      })
+        return ()=>{
+            window.removeEventListener("resize", handleResize)
+        }
+    })
+    useEffect(()=>{
+        setHidden(true)
+
+    }, [router.pathname])
     function showMenu(e){
         if(window.innerWidth >= lg) return
 
-        const siblingEl = e.target.nextElementSibling
-        siblingEl.classList.remove("hidden")
-        siblingEl.style.left= `0px`
+        setHidden(false)
+setLeft
+        setLeft(`0px`)
 
     }
     function addSpacing(e){
         if(window.innerWidth < lg) return
       
             const parentEl = e.target.parentElement
-            parentEl.offsetWidth
-             const siblingEl = e.target.nextElementSibling
-             siblingEl.style.left= `${parentEl.offsetWidth}px`
+            setLeft(`${parentEl.offsetWidth}px`)
     }
-
+    const isHidden = classNames({
+        "hidden": hidden,
+    })
     return (
         <>
     <li className={` ${style.menu} ${style.menu}`} onClick={showMenu} onMouseOver={addSpacing} ref={menu}>{title}</li>
-                <ul className={`${style.submenu} hidden`} ref={subMenu}>
+                <ul className={`${style.submenu} ${isHidden}`} ref={subMenu} style={{left}}>
                     {articles.map(article=>{
                         return (
 
                         <Link href={`/${mainRoute}/Add${article[1]}`} key={article[0]}> 
-                        <li>
+                    <li >
                             <a href="#">{article[0]}</a>
                             </li>
                             </Link>
