aac3cede136cd445cb2dba918e53520ac534a4a6
StR0vErBG <xcreaw1@abv.bg>
2022-05-09 20:05:27 +0200

    adding box shadow to nav

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -3,19 +3,22 @@ import Link from "next/link"
 
 // Nav Components
 import Hamburger from "./navComponents/Hamburger"
-
+import style from "../../styles/navigation/Nav.module.css"
 const Navbar = () => {
     const headerRef = useRef(null)
     return (
-        <header className="bg-primary z-50" ref={headerRef}>
+        <header className={`bg-primary z-50 `} ref={headerRef}>
 
-        <nav className="container flex flex-center items-center relative bg-primary py-3">
+        <nav className={`flex flex-center items-center bg-primary ${style.cShadow}`}>
+            <div className="flex items-center container relative py-3">
+
             <Hamburger headRef={headerRef}/>
                 <ul >
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
                 </ul>
            
+            </div>
         </nav>
         </header>
 
