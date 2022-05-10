2b7bb50aed49386362b27775ebccabd50d49c8dd
StR0vErBG <xcreaw1@abv.bg>
2022-05-10 20:12:15 +0200

    Adding nImg ReactIcons and full navbar

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,24 +1,77 @@
 import React,{useRef} from 'react';
 import Link from "next/link"
+import Image from 'next/image'
 
 // Nav Components
 import Hamburger from "./navComponents/Hamburger"
 import style from "../../styles/navigation/Nav.module.css"
+// Icons and images
+import {AiOutlineUser} from "react-icons/ai"
+import {BsCart3} from "react-icons/bs"
+import {BiHomeAlt} from "react-icons/bi"
+
 const Navbar = () => {
     const headerRef = useRef(null)
     return (
-        <header className={`bg-primary z-50 `} ref={headerRef}>
+        <header className={`z-50 `} ref={headerRef}>
 
-        <nav className={`flex flex-center items-center bg-primary ${style.cShadow}`}>
-            <div className="flex items-center container relative py-3">
+        <nav className={`flex bg-color ${style.cShadow}`}>
+            <div className="flex items-center justify-between container relative bg-color">
+             
 
-            <Hamburger headRef={headerRef}/>
-                <ul >
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                </ul>
-           
+                    <div className="logo flex items-center">
+                    <Hamburger headRef={headerRef}/>
+                    <div className="flex items-center justify-between hidden md:block">
+                    <Image  src="/images/logo.png" width={250} height={50}/>
+
+                    </div>
+                    </div>
+
+            <div className="flex items-center justify-center hidden lg:block">
+                <div className="flex rounded">
+                    <input type="text" className="px-4 py-2 w-80" placeholder="Search..."/>
+                    <button className="flex items-center justify-center px-4 ">
+                        <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                        </svg>
+                    </button>
+                </div>
             </div>
+                <ul className={`${style.list} flex items-center justify-center`}>
+                    <li className="flex items-center justify-center lg:hidden px-4">
+                    <button >
+                        <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                        </svg>
+                    </button>
+                    </li>
+                    
+                <Link href="/">
+                <li className="flex flex-col items-center justify-center px-4 md:hidden">
+                        <div className="text-3xl"><BiHomeAlt className="icon"/></div>
+                        <div className={`text-sm hidden lg:block`}>Вход</div>
+                    </li>
+                    </Link>
+                <Link href="/">
+                <li className="flex flex-col items-center justify-center px-4">
+                        <div className="text-3xl"><AiOutlineUser className="icon"/></div>
+                        <div className={`text-sm hidden lg:block`}>Вход</div>
+                    </li>
+                    </Link>
+
+                <Link href="/about">
+                <li className="flex flex-col items-center justify-center px-4">
+                        <div className="text-3xl"><BsCart3 className="icon"/></div>
+                        <div className={`text-sm hidden lg:block`}>Количка</div>
+                    </li>
+                    </Link>
+                </ul>
+                </div>
+           
         </nav>
         </header>
 
