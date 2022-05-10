6bd9451fd727d7880729da56728e713751363f4f
StR0vErBG <xcreaw1@abv.bg>
2022-05-10 20:18:14 +0200

    Adding spacing

--- components/layouts/navComponents/navLinks.jsx
+++ components/layouts/navComponents/navLinks.jsx
@@ -35,7 +35,7 @@ const NavLinks = () => {
 
 
     return (
-        <ul className="navigation container text-bg-color text-lg">
+        <ul className="navigation container text-bg-color text-lg py-4">
             {dictionary.map(article=>{
             return <NavLinkMenu title={article.title} articles={article.articles} key={article.title} mainRoute={article.mainRoute}/>
             })}
