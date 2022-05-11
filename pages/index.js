0cb2f4afbd57092d5a3762ec1e51bb5cb14ef011
StR0vErBG <xcreaw1@abv.bg>
2022-05-11 13:22:37 +0200

    working carousel and menu

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 import Head from 'next/head'
 
-// import styles from '../styles/Home.module.css'
+
+// Components
+import NavLinks from "../components/layouts/navComponents/NavLinks"
+// Import Swiper React components
+import SwiperPag from "../components/swiperJs/SwiperPag"
+
+const dicSwiper = [
+  {
+    src:"/images/testCarousel.jpg",
+    key: "test carousel for my monday"
+  },
+  {
+    src:"/images/testCarousel.jpg",
+    key: "test carousel for my monday version 2"
+  },
+
+
+]
 export default function Home() {
   return (
     <>
@@ -9,9 +26,20 @@ export default function Home() {
         <meta name="description" content="Онлайн магазин IvdaGeo" />
       </Head>
 
-     <main className="bg-primary mb-auto">
-       <div className="container">
-         Home page
+     <main className="mb-auto h-full">
+       <div className="container w-full ">
+          <section  className="lg:container lg:grid lg:grid-cols-[25%75%] w-full relative">
+          
+                      <section className="z-10  w-full">
+                      <NavLinks isHome={true} />
+                      </section>
+                    <section className="lg:grid-span-8">
+                    <SwiperPag images={dicSwiper}/>
+
+                    </section>
+
+     
+          </section>
        </div>
      </main>
 
