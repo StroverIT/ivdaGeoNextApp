1f059ecbbfa2db41ac4c3acbd95dd3f36bfabf0b
StR0vErBG <xcreaw1@abv.bg>
2022-05-13 16:53:19 +0200

    Creating decimal one above another pricing

---
+++ components/priceStyling/Pricing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DecAbove = ({isPromo, price, priceDec}) => {
+    return (
+        <div  className="flex mt-auto border-t border-gray">
+        <div className={`h-full flex items-center px-2 py-2 ml-auto text-white ${!isPromo ?"bg-primary" :" bg-secondary"}`}>
+        <div className="text-2xl font-semibold">{price}</div>
+        <div className="relative flex flex-col items-center h-full text-sm">
+          <div className="h-0 mt-1 font-semibold">{priceDec}</div>
+          <div className="h-0 mt-[8.8px]">лв.</div>
+          </div>
+
+        </div>
+      </div>
+    );
+}
+
+export default DecAbove;
