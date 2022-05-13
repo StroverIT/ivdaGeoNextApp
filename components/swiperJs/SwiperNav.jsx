654428a9d2fefa37d58e2f7d304e59c2fcdf59c9
StR0vErBG <xcreaw1@abv.bg>
2022-05-13 17:41:20 +0200

    Adding new left and right icon

--- components/swiperJs/SwiperNav.jsx
+++ components/swiperJs/SwiperNav.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {IoMdArrowDropright} from "react-icons/io"
+import {IoMdArrowDropright, IoMdArrowDropleft} from "react-icons/io"
 
 import style from "../../styles/swiperJs/SwiperNav.module.css"
 
 const SwiperNav = () => {
     return (
-        <div className={`${style.swiperNav} z-10 cursor-pointer absolute flex w-full justify-between`}>
+        <div className={`${style.swiperNav} z-10 cursor-pointer absolute flex w-full justify-between text-4xl`}>
 
-        <div className={`${style.swiperPrev} flex items-center justify-center`}><IoMdArrowDropright/></div>
-        <div className={`${style.swiperNext} flex items-center justify-center`}><IoMdArrowDropright/></div>
+        <div className={`${style.swiperPrev} flex items-center justify-center`}>
+            <IoMdArrowDropleft/>
+            </div>
+        <div className={`${style.swiperNext} flex items-center justify-center`}>
+
+            <IoMdArrowDropright/>
+            </div>
+
         </div>
     );
 }
