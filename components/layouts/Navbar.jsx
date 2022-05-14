8711da1831ea49b78241d92d4f09d68104adf4d8
StR0vErBG <xcreaw1@abv.bg>
2022-05-14 14:11:30 +0200

    Fixing color bugs

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
                     {/* Search icon for mobile */}
                     <li className="flex items-center justify-center px-4 md:hidden text-green">
                     <button >
-                        <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                        <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24">
                             <path
                                 d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
