085e5fdc7a45a7a6992e0dbf000203b096724cff
StR0vErBG <xcreaw1@abv.bg>
2022-05-15 09:54:52 +0200

    prettier formatter

--- components/layouts/navComponents/Hamburger.jsx
+++ components/layouts/navComponents/Hamburger.jsx
@@ -1,54 +1,64 @@
 // React and nextJs things
-import React, {useState,useEffect, useRef} from 'react';
-import { useRouter } from 'next/router'
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
 
 // Styles
-import styles from "../../../styles/navigation/Hamburger.module.css"
+import styles from "../../../styles/navigation/Hamburger.module.css";
 // Components
-import NavLinks from "./NavLinks"
+import NavLinks from "./NavLinks";
 
-const Hamburger = ({headRef}) => {
-  const router = useRouter()
+const Hamburger = ({ headRef }) => {
+  const router = useRouter();
 
-  const hamburger = useRef(null)
-  const navLinks = useRef(null)
-  
-  const [isOpen, menuState] = useState(false)
+  const hamburger = useRef(null);
+  const navLinks = useRef(null);
 
+  const [isOpen, menuState] = useState(false);
 
+  useEffect(() => {
+    menuState(false);
+  }, [router.pathname]);
 
-  useEffect(()=>{
-    menuState(false)
-  }, [ router.pathname])
-
-  useEffect(()=>{
-    if(isOpen){
-     hamburger.current.classList.add(styles.open)
-     navLinks.current.classList.add(styles.menuOpen)
-     navLinks.current.style.top = `${headRef.current.offsetHeight}px` 
-     document.body.classList.add("blury")
+  useEffect(() => {
+    if (isOpen) {
+      hamburger.current.classList.add(styles.open);
+      navLinks.current.classList.add(styles.menuOpen);
+      navLinks.current.style.top = `${headRef.current.offsetHeight}px`;
+      document.body.classList.add("blury");
     }
-    if(!isOpen){
-      hamburger.current.classList.remove(styles.open)
-      navLinks.current.classList.remove(styles.menuOpen)
-     document.body.classList.remove("blury")
-
+    if (!isOpen) {
+      hamburger.current.classList.remove(styles.open);
+      navLinks.current.classList.remove(styles.menuOpen);
+      document.body.classList.remove("blury");
     }
-    
-  }, [isOpen])
-    return (
-      <>
-      <div className={`space-y-1 px-2 cursor-pointer ${styles.hamburger} ${router.route == "/" ?"lg:hidden" : ""}`} onClick={()=> menuState(!isOpen)} ref={hamburger}>
-      <div className="block w-5 h-0.5 bg-primary-lighter"></div>
-      <div className="block w-5 h-0.5 bg-primary-lighter"></div>
-      <div className="block w-3 h-0.5 bg-primary-lighter"></div>
-    </div>
-    <div className={`w-full fixed lg:absolute  -z-20  ${styles.navLinks} ${router.route == "/" ?"lg:hidden" : ""} `} ref={navLinks}>
-      <NavLinks />
-    </div>
-    <div className={`blury-bg -z-30 ${!isOpen ? "hidden" : ""}`} onClick={()=> menuState(false)}></div>
+  }, [isOpen]);
+  return (
+    <>
+      <div
+        className={`space-y-1 px-2 cursor-pointer ${styles.hamburger} ${
+          router.route == "/" ? "lg:hidden" : ""
+        }`}
+        onClick={() => menuState(!isOpen)}
+        ref={hamburger}
+      >
+        <div className="block w-5 h-0.5 bg-primary-lighter"></div>
+        <div className="block w-5 h-0.5 bg-primary-lighter"></div>
+        <div className="block w-3 h-0.5 bg-primary-lighter"></div>
+      </div>
+      <div
+        className={`w-full fixed lg:absolute  -z-20  ${styles.navLinks} ${
+          router.route == "/" ? "lg:hidden" : ""
+        } `}
+        ref={navLinks}
+      >
+        <NavLinks />
+      </div>
+      <div
+        className={`blury-bg -z-30 ${!isOpen ? "hidden" : ""}`}
+        onClick={() => menuState(false)}
+      ></div>
     </>
-    );
-}
+  );
+};
 
 export default Hamburger;
