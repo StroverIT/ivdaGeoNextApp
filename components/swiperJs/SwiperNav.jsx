6abc2f48ae56132cad299d60fca41c04349f55a8
StR0vErBG <xcreaw1@abv.bg>
2022-05-15 09:48:00 +0200

    prettier formating

--- components/swiperJs/SwiperNav.jsx
+++ components/swiperJs/SwiperNav.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 // Icons
-import {IoMdArrowDropright, IoMdArrowDropleft} from "react-icons/io"
+import { IoMdArrowDropright, IoMdArrowDropleft } from "react-icons/io";
 // Styles
-import style from "../../styles/swiperJs/SwiperNav.module.css"
+import style from "../../styles/swiperJs/SwiperNav.module.css";
 
 const SwiperNav = () => {
-    return (
-        <div className={`${style.swiperNav} z-10 text-4xl`}>
-
-        <div className={`${style.swiperPrev} flex items-center justify-center cursor-pointer absolute z-10 select-none`}>
-            <IoMdArrowDropleft/>
-            </div>
-        <div className={`${style.swiperNext} flex items-center justify-center cursor-pointer absolute z-10 select-none`}>
-
-            <IoMdArrowDropright/>
-            </div>
-
-        </div>
-    );
-}
+  return (
+    <div className={`${style.swiperNav} z-10 text-4xl`}>
+      <div
+        className={`${style.swiperPrev} flex items-center justify-center cursor-pointer absolute z-10 select-none`}
+      >
+        <IoMdArrowDropleft />
+      </div>
+      <div
+        className={`${style.swiperNext} flex items-center justify-center cursor-pointer absolute z-10 select-none`}
+      >
+        <IoMdArrowDropright />
+      </div>
+    </div>
+  );
+};
 
 export default SwiperNav;
