b968ad009570f41707fecdf59fe8c7b0fc8b1947
StR0vErBG <xcreaw1@abv.bg>
2022-05-16 18:20:45 +0200

    Basic jsx structure

---
+++ pages/account/resetPassword.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// NextJs
+import Head from "next/head";
+
+export default function resetPassword() {
+  return (
+    <>
+      <Head>
+        <title>IvdaGeo reset password</title>
+        <meta name="description" content="Онлайн магазин IvdaGeo" />
+      </Head>
+      <main>
+        <div className="container justify-center grid-cols-2 xl:grid "></div>
+      </main>
+    </>
+  );
+}
