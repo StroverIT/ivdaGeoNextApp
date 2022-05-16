a72546921aa774a27a9e9bcef76e9588fceeeada
StR0vErBG <xcreaw1@abv.bg>
2022-05-16 18:27:22 +0200

    basic strcutrure

---
+++ pages/account/verifyAfterReg.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// NextJs
+import Head from "next/head";
+
+export default function resetPassword() {
+  return (
+    <>
+      <Head>
+        <title>IvdaGeo reset password</title>
+        <meta name="description" content="Онлайн магазин IvdaGeo" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
