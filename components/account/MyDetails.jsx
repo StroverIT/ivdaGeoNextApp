d3fc5a6a37cbfaa192c8b77710bbb020bb398144
StR0vErBG <xcreaw1@abv.bg>
2022-05-17 16:36:29 +0200

    basic structure for details

--- components/account/MyDetails.jsx
+++ components/account/MyDetails.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function MyDetails() {
-  return <div>MyDetails</div>;
+  return (
+    <section>
+      <h2 className="text-3xl ">Детайли на акаунта</h2>
+
+      <div className="divide-y">
+        <section className="">
+          <h4 className="text-xl">Лична информация</h4>
+        </section>
+        <section className="">
+          <h4 className="text-xl">И-мейл адрес</h4>
+        </section>
+        <section className="">
+          <h4 className="text-xl">Парола</h4>
+        </section>
+      </div>
+    </section>
+  );
 }
