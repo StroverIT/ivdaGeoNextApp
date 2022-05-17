eee8ac32eb37a410fc7ce4df305b23187665a57a
StR0vErBG <xcreaw1@abv.bg>
2022-05-17 09:58:30 +0200

    structure

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 // NextJs
 import Head from "next/head";
@@ -7,14 +7,23 @@ import Link from "next/link";
 import Input from "../../components/form/Input";
 
 export default function index() {
+  const content = useRef(null);
+
   return (
     <>
       <Head>
         <title>IvdaGeo my account</title>
         <meta name="description" content="Онлайн магазин IvdaGeo" />
       </Head>
-      <main>
-        <div></div>
+      <main className="mt-5 mb-auto">
+        <div className="container justify-center ">
+          <h3 className="text-3xl">Моят акаунт</h3>
+
+          <div>
+            <aside></aside>
+            <section ref={content}></section>
+          </div>
+        </div>
       </main>
     </>
   );
