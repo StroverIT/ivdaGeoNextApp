97d212632635246aa68fbb077ac1a2a1c4c9edb2
StR0vErBG <xcreaw1@abv.bg>
2022-05-17 09:53:57 +0200

    basic structure

---
+++ pages/account/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// NextJs
+import Head from "next/head";
+import Link from "next/link";
+// Components
+import Input from "../../components/form/Input";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <title>IvdaGeo my account</title>
+        <meta name="description" content="Онлайн магазин IvdaGeo" />
+      </Head>
+      <main>
+        <div></div>
+      </main>
+    </>
+  );
+}
