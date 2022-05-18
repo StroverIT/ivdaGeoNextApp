7793977ab09a34ef4d06d0d8b854e77580a576bc
StR0vErBG <xcreaw1@abv.bg>
2022-05-18 23:36:33 +0200

    chaning to react syntaxis

--- components/form/AccInput.jsx
+++ components/form/AccInput.jsx
@@ -5,17 +5,17 @@ import Icons from "../Icons/Icons";
 // Get data from the user and fullfil in inputplaceholder
 const AccInput = ({ type, placeholder, id, isReq, iconType, data }) => {
   return (
-    <div class="w-full max-w-xs">
-      <div class="mb-4">
+    <div className="w-full max-w-xs">
+      <div className="mb-4">
         <label
-          class="block text-gray-700 text-sm  mb-1 font-semibold"
-          for="username"
+          className="block mb-1 text-sm font-semibold text-gray-700"
+          htmlFor="username"
         >
           {placeholder}
         </label>
         <div>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
             id={id}
             name={id}
             type={type}
