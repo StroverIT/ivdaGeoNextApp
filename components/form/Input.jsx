45474d87f344a298e13a56f0fc1b04d1cf1862a3
StR0vErBG <xcreaw1@abv.bg>
2022-05-18 16:52:43 +0200

    Making some changes

--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-// Icons
-import { HiOutlineMail, HiOutlineUserCircle } from "react-icons/hi";
-import { AiOutlineLock } from "react-icons/ai";
+
+import Icons from "../Icons/Icons";
 
 const Input = ({ type, placeholder, id, isReq, iconType }) => {
-  const iconDictionary = {
-    password: <AiOutlineLock />,
-    email: <HiOutlineMail />,
-    fullName: <HiOutlineUserCircle />,
-  };
   return (
     <div className="relative mb-5">
       <div className="absolute left-0 z-10 top-2.5 text-lg">
-        {iconDictionary[iconType]}
+        <Icons iconType={iconType} />
       </div>
       <input
         className="w-full px-6 py-2 leading-tight placeholder-transparent border-b appearance-none placeholder:pl-10 peer text-gray-darker focus:outline-none focus:shadow-outline"
