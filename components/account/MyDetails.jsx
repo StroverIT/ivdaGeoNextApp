0d6d12ebdf685666a6755577a73bf9dec35148df
StR0vErBG <xcreaw1@abv.bg>
2022-05-18 16:43:58 +0200

    template for html

--- components/account/MyDetails.jsx
+++ components/account/MyDetails.jsx
@@ -10,12 +10,64 @@ export default function MyDetails() {
       <div className="divide-y">
         <section className="">
           <h4 className="text-xl">Лична информация</h4>
+          <form action="">
+            <Input
+              placeholder="Пълно име"
+              iconType="fullName"
+              isReq={true}
+              type="text"
+              id="fullname"
+            />
+            <Input
+              placeholder="Телефонен номер"
+              id="phoneNumber"
+              isReq={true}
+              iconType="phoneNumber"
+            />
+          </form>
         </section>
         <section className="">
           <h4 className="text-xl">И-мейл адрес</h4>
+          <form action="">
+            <Input
+              placeholder="И-мейл адрес"
+              iconType="email"
+              id="email"
+              type="email"
+              isReq={true}
+            />
+            <Input
+              placeholder="Парола"
+              iconType="password"
+              id="password"
+              type="password"
+              isReq={true}
+            />
+          </form>
         </section>
         <section className="">
           <h4 className="text-xl">Парола</h4>
+          <Input
+            placeholder="Сегашна парола"
+            id="currentPassword"
+            type="text"
+            isReq={true}
+            iconType="password"
+          />
+          <Input
+            placeholder="Нова парола"
+            id="newPassword"
+            type="password"
+            isReq={true}
+            iconType="password"
+          />
+          <Input
+            placeholder="Потвърди новата парола"
+            id="newPasswordConf"
+            type="password"
+            isReq={true}
+            iconType="password"
+          />
         </section>
       </div>
     </section>
