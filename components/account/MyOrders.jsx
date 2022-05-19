1207532c9d0a64de0858ab6bfcfd066abc08c73c
StR0vErBG <xcreaw1@abv.bg>
2022-05-19 22:59:11 +0200

    adding table and sample template

--- components/account/MyOrders.jsx
+++ components/account/MyOrders.jsx
@@ -12,6 +12,32 @@ export default function MyOrders() {
       <div className="text-center pb-10 text-secondary">
         Нямате направени поръчки
       </div>
+      <table class="table-auto w-full">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+          </tr>
+          <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+          </tr>
+          <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 }
