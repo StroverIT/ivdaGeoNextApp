046acf0c6a40ea41cf915556250dd08a32fc1fb8
StR0vErBG <xcreaw1@abv.bg>
2022-05-20 22:34:31 +0200

    removing max width

--- components/form/AccInput.jsx
+++ components/form/AccInput.jsx
@@ -5,7 +5,7 @@ import Icons from "../Icons/Icons";
 // Get data from the user and fullfil in inputplaceholder
 const AccInput = ({ type, placeholder, id, isReq, iconType, data }) => {
   return (
-    <div className="w-full max-w-xs">
+    <div className="w-full">
       <div className="mb-4">
         <label
           className="block mb-1 text-sm font-semibold text-gray-700"
