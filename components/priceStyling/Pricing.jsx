cf099ed9de946dedee8d4f72c905e683f28054bf
StR0vErBG <xcreaw1@abv.bg>
2022-05-20 21:49:28 +0200

    chaning font-sizing

--- components/priceStyling/Pricing.jsx
+++ components/priceStyling/Pricing.jsx
@@ -4,8 +4,8 @@ export default function Pricing({ price, priceDec }) {
   return (
     <div className="flex">
       <div className={`flex items-center px-2 py-2  `}>
-        <div className="text-2xl font-semibold">{price}</div>
-        <div className="relative flex flex-col items-center h-full text-sm">
+        <div className="text-xl font-semibold">{price}</div>
+        <div className="relative flex flex-col items-center h-full text-xs">
           <div className="h-0 mt-1 font-semibold">{priceDec}</div>
           <div className="h-0 mt-[8.8px]">лв.</div>
         </div>
