e809dbe97652e13c4bf0089b1bb83b750e3f3553
StR0vErBG <xcreaw1@abv.bg>
2022-05-21 15:53:44 +0200

    fixing width and responsivness

--- components/account/MyDetails/Description.jsx
+++ components/account/MyDetails/Description.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Description({ text }) {
   return (
-    <section className="text-sm text-gray-darker">
+    <section className="text-sm text-gray-darker text-center sm:w-2/3 md:w-4/5 md:text-left">
       <p>{text}</p>
     </section>
   );
