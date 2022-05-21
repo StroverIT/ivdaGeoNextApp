6f05b59941ed5b54cd2e73799e4f69109787b45f
StR0vErBG <xcreaw1@abv.bg>
2022-05-21 15:54:37 +0200

    fixing gap

--- components/account/MyDetails/SectionContainer.jsx
+++ components/account/MyDetails/SectionContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Section({ content }) {
   return (
-    <section className="flex flex-col justify-center md:justify-between items-center md:items-start md:grid grid-cols-[45%55%] py-3 gap-5 w-full">
+    <section className="flex flex-col justify-center md:justify-between items-center md:items-start md:grid grid-cols-[45%55%] py-3 gap-5 md:gap-0 w-full">
       {content}
     </section>
   );
