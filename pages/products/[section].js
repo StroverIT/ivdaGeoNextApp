52bfe95e9bf0b39a872e24b361ac053fa704b6be
StR0vErBG <xcreaw1@abv.bg>
2022-05-22 22:00:57 +0200

    Basic structure

--- pages/products/[section].js
+++ pages/products/[section].js
@@ -5,6 +5,9 @@ import { useRouter } from "next/router";
 export default function section() {
   const router = useRouter();
   const { section } = router.query;
-  console.log(section);
-  return <div>test</div>;
+  return (
+    <main className="mb-auto">
+      <div className="container"></div>
+    </main>
+  );
 }
