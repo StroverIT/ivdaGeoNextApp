44b9097a2b91cfc6f5916021a5e16d8672692520
StR0vErBG <xcreaw1@abv.bg>
2022-05-22 21:57:28 +0200

    adding router for router query

--- pages/products/[section].js
+++ pages/products/[section].js
@@ -3,5 +3,8 @@ import React from "react";
 import { useRouter } from "next/router";
 
 export default function section() {
+  const router = useRouter();
+  const { section } = router.query;
+  console.log(section);
   return <div>test</div>;
 }
