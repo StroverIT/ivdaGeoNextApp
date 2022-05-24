412d91606afe19b635b25490a9ba096087f112ba
StR0vErBG <xcreaw1@abv.bg>
2022-05-24 22:17:50 +0200

    Adding some dynamic

--- components/products/Checkbox.jsx
+++ components/products/Checkbox.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 
-export default function Checkbox() {
+export default function Checkbox({ text, quanity, id }) {
   return (
     <>
       <input
         className="w-4 h-4 mt-1 mr-2 align-top border-gray accent-primary-lighter"
         type="checkbox"
-        id="blackAndDecker"
+        id={id}
         value="option1"
       />
-      <label
-        className="inline-block select-none form-check-label"
-        htmlFor="blackAndDecker"
-      >
-        BLACK&DECKER
-        <span>{/*quantity */}</span>
+      <label className="inline-block select-none form-check-label" htmlFor={id}>
+        {text}
+        <span>{quanity}</span>
       </label>
     </>
   );
