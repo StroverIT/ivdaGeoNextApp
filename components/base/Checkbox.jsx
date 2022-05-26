e60d55a61b62060dcf8877aee0d30a1919fb4d36
StR0vErBG <xcreaw1@abv.bg>
2022-05-26 23:03:00 +0200

    fixing checkbox bug

--- components/base/Checkbox.jsx
+++ components/base/Checkbox.jsx
@@ -4,13 +4,13 @@ export default function Checkbox({ text, quantity, id }) {
   return (
     <div className="form-check">
       <input
-        className="float-left w-4 h-4 mt-1 mr-2 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border border-gray-300 rounded-sm appearance-none cursor-pointer form-check-input checked:bg-blue-600 checked:border-blue-600 focus:outline-none"
+        className="float-left w-4 h-4 mt-1 mr-2 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border border-gray-300 rounded-sm appearance-none cursor-pointer form-check-input checked:bg-primary checked:border-primary focus:outline-none border-gray"
         type="checkbox"
         id={id}
         value="option1"
       />
       <label
-        className="inline-block font-semibold text-gray-800 select-none form-check-label "
+        className="inline-block font-normal text-gray-800 select-none form-check-label "
         htmlFor={id}
       >
         {text}
