ab427358aeece7619cce2ff1bdb83d07cf390eff
StR0vErBG <xcreaw1@abv.bg>
2022-05-27 18:58:11 +0200

    Fully created sorting

---
+++ components/products/filters/Option.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Option({ value, text }) {
+  return <option value={value}>{text}</option>;
+}
--- components/products/filters/Sorting.jsx
+++ components/products/filters/Sorting.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
-export default function Sorting() {
+import Option from "./Option";
+
+function Quantity({ qty }) {
+  return <span className="pl-1 text-sm text-primary-lighter">{qty}</span>;
+}
+export default function Sorting({ title, name, data, qty }) {
+  console.log(data);
   return (
-    <div className="flex">
-      <label htmlFor="sortBy">Сортирай:</label>
+    <div className="flex text-[#888] items-center justify-center">
+      <label htmlFor={name} className="text-sm">
+        {title}
+        {qty && <Quantity qty={qty} />}:
+      </label>
       <div>
-        <select name="sortBy" id="sortBy">
-          <option value="nameAsc">Име (възходящ ред)</option>
-          <option value="nameDesc">Име (низходящ ред)</option>
-          <option value="priceAsc">Цена (възходящ ред)</option>
-          <option value="priceDesc">Цена (низходящ ред)</option>
+        <select name={name} id={name} className="border border-gray p-1 ml-2">
+          {data.map((e) => {
+            return <Option value={e.val} text={e.text} key={e.val} />;
+          })}
         </select>
       </div>
-      <input type="text" />
     </div>
   );
 }
