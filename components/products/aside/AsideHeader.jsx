c7119e9262787073550b5d3508d405b75837fcdf
StR0vErBG <xcreaw1@abv.bg>
2022-05-27 06:55:35 +0200

    Basic styling

--- components/products/aside/AsideHeader.jsx
+++ components/products/aside/AsideHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AsideHeader({ text }) {
   return (
-    <div className="relative filter-container">
+    <div className="relative my-5 filter-container">
       <h3 className="filter-header">{text}</h3>
     </div>
   );
