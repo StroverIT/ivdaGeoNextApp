297e72ca75a46a97e52c05bc6b5207a063be3524
StR0vErBG <xcreaw1@abv.bg>
2022-05-28 05:25:02 +0200

    FIxing the bug

--- components/swiperJs/ThumbsGallery.jsx
+++ components/swiperJs/ThumbsGallery.jsx
@@ -25,12 +25,14 @@ export default function App() {
         }}
         spaceBetween={10}
         navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
         modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper2"
       >
-        <SwiperSlide className="responsive">
-          <Image src="/images/testCarousel.jpg" layout="fill" />
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
--- pages/products/[section]/[product]/index.js
+++ pages/products/[section]/[product]/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import ThumbsGallery from "../../../../components/swiperJs/ThumbsGallery";
+
 export default function index() {
   return (
     <main className="mb-auto">
-      <div className="container"></div>
+      <div className="container">
+        <ThumbsGallery />
+      </div>
     </main>
   );
 }
