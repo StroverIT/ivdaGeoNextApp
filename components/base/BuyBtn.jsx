5113ce3a218bf1fa2e30f90818a2a193a76cb7e9
StR0vErBG <xcreaw1@abv.bg>
2022-05-28 09:46:00 +0200

    adding dictionary and more flexible btn

--- components/base/BuyBtn.jsx
+++ components/base/BuyBtn.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-export default function BuyBtn({ border }) {
+const sizing = {
+  lg: " px-5 md:px-16 py-5 md:text-xl",
+};
+export default function BuyBtn({ border, lg }) {
   return (
     <div className={`${border ? "border-t border-gray" : ""}`}>
       <button
         type="button"
-        className="w-full py-1 my-10 font-semibold text-white rounded-full bg-primary lg:my-5"
+        className={`${
+          lg ? sizing.lg : ""
+        } w-full py-1 my-10 font-semibold text-white rounded-full bg-primary lg:my-5`}
       >
         Купи
       </button>
