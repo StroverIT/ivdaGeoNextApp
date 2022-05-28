6d4b78e34b25d8400db095536e291ab5504acd8b
StR0vErBG <xcreaw1@abv.bg>
2022-05-28 11:08:50 +0200

    Addin container

--- components/swiperJs/ThumbsGallery.jsx
+++ components/swiperJs/ThumbsGallery.jsx
@@ -18,90 +18,93 @@ export default function App() {
 
   return (
     <>
-      <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-        }}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{
-          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-        }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        <SwiperSlide>
-          <div className="relative w-full h-52">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-52">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-52">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-52">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-52">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        spaceBetween={10}
-        slidesPerView={2}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        breakpoints={{
-          // when window width is >= 640px
-          670: {
-            slidesPerView: 3,
-          },
-          950: {
-            slidesPerView: 4,
-          },
-          // when window width is >= 768px
-        }}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="relative w-full h-24">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-24">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-24">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-24">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative w-full h-24">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className="swiperContainer">
+        <Swiper
+          style={{
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          }}
+          spaceBetween={10}
+          navigation={true}
+          thumbs={{
+            swiper:
+              thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+          }}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="mySwiper2"
+        >
+          <SwiperSlide>
+            <div className="relative w-full h-52 lg:h-40">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-52 lg:h-40">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-52 lg:h-40">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-52 lg:h-40">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-52 lg:h-40">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView={2}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          breakpoints={{
+            // when window width is >= 640px
+            670: {
+              slidesPerView: 3,
+            },
+            950: {
+              slidesPerView: 4,
+            },
+            // when window width is >= 768px
+          }}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="relative w-full h-24">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-24">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-24">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-24">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative w-full h-24">
+              <Image src="/images/testCarousel.jpg" layout="fill" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 }
