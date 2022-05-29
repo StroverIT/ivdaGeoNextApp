7b5fd433f01b0153140b0a37d54581a7d649a490
StR0vErBG <xcreaw1@abv.bg>
2022-05-29 08:50:02 +0200

    Adding sizing

--- components/priceStyling/Pricing.jsx
+++ components/priceStyling/Pricing.jsx
@@ -12,9 +12,13 @@ const sizeDictionary = {
 export default function Pricing({ price, priceDec, size }) {
   return (
     <div className="flex">
-      <div className={`flex items-center px-2 py-2 ${size} `}>
-        <div className={` font-semibold ${size}`}>{price}</div>
-        <div className="relative flex flex-col items-center h-full text-xs">
+      <div className={`flex items-center px-2 py-2 } `}>
+        <div className={` font-semibold ${sizeDictionary[size][0]}`}>
+          {price}
+        </div>
+        <div
+          className={`relative flex flex-col items-center h-full ${sizeDictionary[size][1]}`}
+        >
           <div className="h-0 mt-1 font-semibold">{priceDec}</div>
           <div className="h-0 mt-[8.8px]">лв.</div>
         </div>
