b2ff5f1dd484419c70e6d089f447bd102b1bd11d
StR0vErBG <xcreaw1@abv.bg>
2022-05-29 08:55:36 +0200

    fixing spacing

--- components/priceStyling/Pricing.jsx
+++ components/priceStyling/Pricing.jsx
@@ -7,9 +7,9 @@ import React from "react";
 3. text-{something} on priceDec
 */
 const sizeDictionary = {
-  xl: ["text-xl", "text-xs"],
-  "2xl": ["text-2xl", "text-sm"],
-  "3xl": ["text-3xl", "text-sm"],
+  xl: ["text-xl", "text-xs", "mt-[9px]"],
+  "2xl": ["text-2xl", "text-sm", "mt-[9px]"],
+  "3xl": ["text-3xl", "text-sm", "mt-[11px]"],
 };
 export default function Pricing({ price, priceDec, size }) {
   return (
@@ -19,10 +19,10 @@ export default function Pricing({ price, priceDec, size }) {
           {price}
         </div>
         <div
-          className={`relative flex flex-col items-center h-full ${sizeDictionary[size][1]}`}
+          className={`relative flex flex-col items-center h-full ${sizeDictionary[size][1]} `}
         >
           <div className="h-0 mt-1 font-semibold">{priceDec}</div>
-          <div className="h-0 mt-[9px]">лв.</div>
+          <div className={`h-0 ${sizeDictionary[size][2]}`}>лв.</div>
         </div>
       </div>
     </div>
