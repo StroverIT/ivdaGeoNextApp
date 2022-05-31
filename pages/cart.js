8b65025592e342c85a2575910eb6a4fb90153e93
StR0vErBG <xcreaw1@abv.bg>
2022-05-31 14:23:37 +0200

    basic structure

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,17 @@
+// NextJs
 import Head from "next/head";
+import Image from "next/image";
+// Components
+import Price from "../components/priceStyling/Pricing";
+import QunityInput from "../components/base/QuanityInput";
 
+function TableData({ children }) {
+  return (
+    <td className="" colSpan="1">
+      {children}
+    </td>
+  );
+}
 // import styles from '../styles/Home.module.css'
 export default function Cart() {
   return (
@@ -9,8 +21,51 @@ export default function Cart() {
         <meta name="description" content="Онлайн магазин IvdaGeo" />
       </Head>
 
-      <main className="bg-primary">
-        <div className="container"></div>
+      <main className=" mb-auto">
+        <div className="container">
+          <h3 className="text-3xl uppercase font-semibold">Твоята количка</h3>
+          <table className="table-fixed w-full">
+            <thead>
+              <tr>
+                <th colSpan="2">Продукт</th>
+                <th colSpan="1">Цена</th>
+                <th colSpan="1">Количество</th>
+                <th colSpan="1">Общо</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <TableData>
+                  <div className="relative h-10 w-10 ">
+                    <Image layout="fill" src="/images/testCarousel.jpg" />
+                  </div>
+                </TableData>
+                <TableData>
+                  <h3 className="text-xl">
+                    АКУМУЛАТОРЕН КОМПЛЕКТ EINHELL TE-TK 12 Li
+                  </h3>
+                  <ul>
+                    <li>ИНСТРУМЕНТ 1 : АКУМУЛАТОРНА БОРМАШИНА</li>
+                    <li>ИНСТРУМЕНТ 2 : МУЛТИФУНКЦИОНАЛНО УСТРОЙСТВО</li>
+                    <li>
+                      ИНСТРУМЕНТ 3 : АКУМУЛАТОРЕН ФЕНЕР ТИП БАТЕРИЯ : Li-Ion
+                    </li>
+                    <li>НАПРЕЖЕНИЕ : 12.00 V</li>
+                  </ul>
+                </TableData>
+                <TableData>
+                  <Price priceDec={20} price={100} size="2xl" />
+                </TableData>
+                <TableData>
+                  <QunityInput />
+                </TableData>
+                <TableData>
+                  <Price priceDec={48} price={200} size="2xl" />
+                </TableData>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </>
   );
