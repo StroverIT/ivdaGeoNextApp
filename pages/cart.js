8f288d7add3a421f9bac7e52d0f9e4f65f965a4e
StR0vErBG <xcreaw1@abv.bg>
2022-05-31 18:55:06 +0200

    fixing spacing and some styling issues

--- pages/cart.js
+++ pages/cart.js
@@ -26,7 +26,7 @@ export default function Cart() {
       <main className="mb-auto ">
         <div className="container">
           <h3 className="text-3xl font-semibold uppercase">Твоята количка</h3>
-          <div>
+          <div className="xl:grid grid-cols-[70%30%]">
             <table className="w-full table-auto">
               <thead className="bg-gray-100 text-gray-250">
                 <tr>
@@ -44,23 +44,27 @@ export default function Cart() {
                     </div>
                   </TableData>
                   <TableData>
-                    <h3 className="text-lg">
+                    <h3 className="">
                       АКУМУЛАТОРЕН КОМПЛЕКТ EINHELL TE-TK 12 Li
                     </h3>
-                    <ul className="mt-2 text-sm text-gray-250">
-                      <li>ИНСТРУМЕНТ 1 : АКУМУЛАТОРНА БОРМАШИНА</li>
-                      <li>ИНСТРУМЕНТ 2 : МУЛТИФУНКЦИОНАЛНО УСТРОЙСТВО</li>
-                      <li>
+                    <ul className="mt-2 text-xs text-gray-250">
+                      <li className="pb-1">
+                        ИНСТРУМЕНТ 1 : АКУМУЛАТОРНА БОРМАШИНА
+                      </li>
+                      <li className="pb-1">
+                        ИНСТРУМЕНТ 2 : МУЛТИФУНКЦИОНАЛНО УСТРОЙСТВО
+                      </li>
+                      <li className="pb-1">
                         ИНСТРУМЕНТ 3 : АКУМУЛАТОРЕН ФЕНЕР ТИП БАТЕРИЯ : Li-Ion
                       </li>
-                      <li>НАПРЕЖЕНИЕ : 12.00 V</li>
+                      <li className="pb-1">НАПРЕЖЕНИЕ : 12.00 V</li>
                     </ul>
                   </TableData>
                   <TableData>
                     <Price priceDec={20} price={100} size="2xl" />
                   </TableData>
                   <TableData>
-                    <QunityInput contClass="w-1/3 mx-auto mt-10" />
+                    <QunityInput contClass="w-1/2 mx-auto mt-10" />
                     <div className="flex items-center justify-center mt-2">
                       <button
                         type="button"
