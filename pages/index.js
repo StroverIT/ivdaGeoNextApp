2049e30e4ae88f7de9318832d74086e253c35675
StR0vErBG <xcreaw1@abv.bg>
2022-06-01 10:20:36 +0200

    REMOVING COPIED NEWSLATTER

--- pages/index.js
+++ pages/index.js
@@ -110,40 +110,7 @@ export default function Home() {
           </div>
         </section>
         <section className="pt-10 pb-2 bg-color">
-          <div className="container h-full">
-            <div className="justify-between lg:flex ">
-              <section className="flex flex-col items-center justify-center w-full p-3 sm:flex-row sm:justify-start bg-green custom-border">
-                <div className="">
-                  <Image src="/images/logo.png" width="280" height="70" />
-                </div>
-                <div className="w-full mt-2 text-center text-white sm:mt-0 sm:ml-4 lg:text-left">
-                  АБОНИРАЙТЕ СЕ ЗА НАШИТЕ СПЕЦИАЛНИ ОФЕРТИ
-                </div>
-              </section>
-
-              <div className="flex items-center justify-center w-full mt-5 lg:mt-0">
-                <div className="flex justify-end w-full">
-                  <input
-                    type="text"
-                    className="w-full px-4 py-2 border lg:w-64 border-gray"
-                    placeholder="Вашият и-мейл"
-                  />
-                  <button
-                    type="button"
-                    className="px-4 text-white transition-colors w-52 bg-primary hover:bg-primary-trans"
-                  >
-                    Абонирай ме
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="my-5 text-sm text-center">
-              С абонамента се съгласявате предоставените от Вас лични данни да
-              бъдат обработвани за целите и по начина посочени в общите условия,
-              включително, но не само, за да получавате информация за актуални
-              промоции и други активности на ИвдаГео.
-            </div>
-          </div>
+          <div className="container h-full"></div>
         </section>
         <section className="py-5 ">
           <div className="container">
