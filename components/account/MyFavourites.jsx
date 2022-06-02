92a60e41eb7dae37fc2d3beae338a0e094eb6e91
StR0vErBG <xcreaw1@abv.bg>
2022-06-02 20:20:00 +0200

    fixing the layout

--- components/account/MyFavourites.jsx
+++ components/account/MyFavourites.jsx
@@ -2,12 +2,42 @@ import React from "react";
 
 // Icons
 import { BsTrash } from "react-icons/bs";
+// Styles
+import style from "../../styles/account/myFavourites.module.css";
 
 // NextJs
 import Image from "next/image";
 // Components
 import Pricing from "../priceStyling/Pricing";
 
+function Card() {
+  return (
+    <div className="w-full">
+      <div className="relative w-full h-48">
+        <Image src="/images/testImage2.png" layout="fill" objectFit="contain" />
+        <div className="absolute right-0 top-0 cursor-pointer text-dark bg-white rounded-full p-2 text-xl border border-gray-200 hover:scale-105 hover:text-primary transition-all">
+          <BsTrash />
+        </div>
+      </div>
+      <div>
+        <h5 className="font-semibold text-sm">
+          АКУМУЛАТОРЕН КОМПЛЕКТ EINHELL TE-TK 12 Li
+        </h5>
+      </div>
+      <div className="pb-2 pt-1">
+        <Pricing price={20} priceDec={10} size="sm" />
+      </div>
+      <div>
+        <button
+          type="button"
+          class="bg-primary text-white py-2 w-full text-xs font-semibold"
+        >
+          ДОБАВИ В КОЛИЧКА
+        </button>
+      </div>
+    </div>
+  );
+}
 export default function MyFavourites() {
   return (
     <section>
@@ -17,35 +47,16 @@ export default function MyFavourites() {
       <div className="text-center text-secondary pb-10">
         Не сте сложили любими продукти
       </div>
-      <section className="flex">
-        <div className="w-1/3">
-          <div className="relative w-full h-48">
-            <Image
-              src="/images/testImage2.png"
-              layout="fill"
-              objectFit="contain"
-            />
-            <div className="absolute right-0 top-0 cursor-pointer text-dark bg-white rounded-full p-2 text-xl border border-gray-200 hover:scale-105 hover:text-primary transition-all">
-              <BsTrash />
-            </div>
-          </div>
-          <div>
-            <h5 className="font-semibold text-sm">
-              АКУМУЛАТОРЕН КОМПЛЕКТ EINHELL TE-TK 12 Li
-            </h5>
-          </div>
-          <div className="pb-2 pt-1">
-            <Pricing price={20} priceDec={10} size="sm" />
-          </div>
-          <div>
-            <button
-              type="button"
-              class="bg-primary text-white py-2 w-full text-xs font-semibold"
-            >
-              ДОБАВИ В КОЛИЧКА
-            </button>
-          </div>
-        </div>
+      <section
+        className={` flex-wrap gap-5 justify-center grid ${style.itemsContainer}`}
+      >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </section>
   );
