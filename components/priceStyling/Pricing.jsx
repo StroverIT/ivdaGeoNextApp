bba44d75b73efd9e9f81f6242f91c830dc0c5f2c
StR0vErBG <xcreaw1@abv.bg>
2022-06-02 19:34:22 +0200

    removing unnescessery layout

--- components/priceStyling/Pricing.jsx
+++ components/priceStyling/Pricing.jsx
@@ -14,7 +14,7 @@ const sizeDictionary = {
 };
 export default function Pricing({ price, priceDec, size }) {
   return (
-    <div className="flex justify-center">
+    <div className="flex">
       <div className={`flex items-center px-2  } `}>
         <div className={`font-semibold ${sizeDictionary[size][0]}`}>
           {price}
