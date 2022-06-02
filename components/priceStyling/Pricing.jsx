daedd3b29628d676b1e09b49e6073f82ed3bef52
StR0vErBG <xcreaw1@abv.bg>
2022-06-02 19:33:02 +0200

    adding sm sizing

--- components/priceStyling/Pricing.jsx
+++ components/priceStyling/Pricing.jsx
@@ -7,6 +7,7 @@ import React from "react";
 3. text-{something} on priceDec
 */
 const sizeDictionary = {
+  sm: ["text-xl", "text-[11px]", "mt-[7px]"],
   xl: ["text-xl", "text-xs", "mt-[9px]"],
   "2xl": ["text-2xl", "text-sm", "mt-[9px]"],
   "3xl": ["text-3xl", "text-sm", "mt-[11px]"],
@@ -15,7 +16,7 @@ export default function Pricing({ price, priceDec, size }) {
   return (
     <div className="flex justify-center">
       <div className={`flex items-center px-2 py-2 } `}>
-        <div className={` font-semibold ${sizeDictionary[size][0]}`}>
+        <div className={`font-semibold ${sizeDictionary[size][0]}`}>
           {price}
         </div>
         <div
