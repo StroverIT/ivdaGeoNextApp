e24d9c41b150992a183d33fc7cf385985c976144
StR0vErBG <xcreaw1@abv.bg>
2022-06-02 09:40:39 +0200

    removing unnecessary things

--- components/priceStyling/PricingPromo.jsx
+++ components/priceStyling/PricingPromo.jsx
@@ -1,20 +1,11 @@
 // React
 import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-// Next
-import Link from "next/link";
 
-const Pricing = ({ isPromo, price, priceDec, pageUrl }) => {
+const Pricing = ({ isPromo, price, priceDec }) => {
   return (
     <div className="flex mt-auto border-t border-gray">
-      <Link href={pageUrl}>
-        <div className="flex items-center justify-center cursor-pointer ml-7">
-          <AiOutlineSearch />
-          <span className="ml-1 text-sm">Виж</span>
-        </div>
-      </Link>
       <div
-        className={`h-full flex items-center px-2 py-2 ml-auto text-white ${
+        className={` flex items-center px-2 py-2 ml-auto text-white ${
           !isPromo ? "bg-primary" : " bg-secondary"
         }`}
       >
