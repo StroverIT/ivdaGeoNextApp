21172b77a0554a6b6354ed3cdf34ac011270cf02
StR0vErBG <xcreaw1@abv.bg>
2022-06-02 20:36:19 +0200

    fixing spacing

--- components/priceStyling/Pricing.jsx
+++ components/priceStyling/Pricing.jsx
@@ -10,7 +10,7 @@ const sizeDictionary = {
   sm: ["text-xl", "text-[11px]", "mt-[2px]", "mt-[0.2px]", "mt-[7px]"],
   xl: ["text-xl", "text-xs", "mt-[1px]", "mt-[1px]", "mt-[9px]"],
   "2xl": ["text-2xl", "text-sm", "mt-[1px]", "mt-[1px]", "mt-[9px]"],
-  "3xl": ["text-3xl", "text-sm", "mt-[1px]", "mt-[1px]", "mt-[11px]"],
+  "3xl": ["text-3xl", "text-sm", "mt-[2px]", "mt-[1px]", "mt-[11px]"],
 };
 export default function Pricing({ price, priceDec, size }) {
   return (
