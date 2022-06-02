ad2919966b66ced68994da749253f1726e28c23c
StR0vErBG <xcreaw1@abv.bg>
2022-06-02 19:56:30 +0200

    adding more styling to trash :D

--- components/account/MyFavourites.jsx
+++ components/account/MyFavourites.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Icons
+import { BsTrash } from "react-icons/bs";
+
 // NextJs
 import Image from "next/image";
 // Components
@@ -15,16 +18,25 @@ export default function MyFavourites() {
         Не сте сложили любими продукти
       </div>
       <section className="flex">
-        <div>
-          <div className="relative w-56 h-48">
-            <Image src="/images/testCarousel.jpg" layout="fill" />
+        <div className="w-1/3">
+          <div className="relative w-full h-48">
+            <Image
+              src="/images/testImage2.png"
+              layout="fill"
+              objectFit="contain"
+            />
+            <div className="absolute right-0 top-0 cursor-pointer text-dark bg-white rounded-full p-2 text-xl border border-gray-200 hover:scale-105 hover:text-primary transition-all">
+              <BsTrash />
+            </div>
           </div>
           <div>
             <h5 className="font-semibold text-sm">
               АКУМУЛАТОРЕН КОМПЛЕКТ EINHELL TE-TK 12 Li
             </h5>
           </div>
-          <Pricing price={20} priceDec={10} size="sm" />
+          <div className="pb-2 pt-1">
+            <Pricing price={20} priceDec={10} size="sm" />
+          </div>
           <div>
             <button
               type="button"
