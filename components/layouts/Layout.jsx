2bb1421dc9313bd26997a68fcc96f3eb0c327e59
StR0vErBG <xcreaw1@abv.bg>
2022-06-03 20:27:07 +0200

    fixing height

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col justify-between">
+    <div className="flex flex-col justify-between min-h-screen">
       <Navbar />
       {children}
       <Footer />
