20dfcad246ef4e682ebd86b1ba4a1819fc577bf5
StR0vErBG <xcreaw1@abv.bg>
2022-06-03 05:15:10 +0200

    fully working search menu

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -11,9 +11,14 @@ import { BsCart3 } from "react-icons/bs";
 
 const Navbar = () => {
   const headerRef = useRef(null);
+  const searchMenu = useRef(null);
+
   const [fixed, setFixed] = useState(false);
   const [show, setShow] = useState(null);
   const [lastScrollY, setLastScrollY] = useState(0);
+
+  const [showSearch, setShowSearch] = useState(false);
+
   const controlNavbar = () => {
     if (window.scrollY > lastScrollY) {
       setShow(false);
@@ -45,7 +50,7 @@ const Navbar = () => {
     >
       <nav className={`flex bg-color ${style.cShadow}`}>
         <div className="container relative flex items-center justify-between ">
-          <div className="flex items-center logo justify-center">
+          <div className="flex items-center justify-center logo">
             <Hamburger headRef={headerRef} />
             <Link href="/">
               <div className="items-center justify-between hidden lg:flex">
@@ -57,8 +62,11 @@ const Navbar = () => {
                 />
               </div>
             </Link>
-            <li className="flex items-center justify-center px-2 lg:hidden hover:text-white hover:bg-gray h-14 ">
-              <button>
+            <li
+              className="flex items-center justify-center px-2 lg:hidden hover:text-white hover:bg-gray h-14 "
+              onClick={() => setShowSearch(!showSearch)}
+            >
+              <button type="button" className="w-full h-full">
                 <svg
                   className="w-6 h-6"
                   fill="currentColor"
@@ -85,7 +93,10 @@ const Navbar = () => {
 
           <ul className={`${style.list} flex items-center justify-center `}>
             {/* Search icon */}
-            <li className="items-center justify-center hidden px-4 lg:flex h-14 lg:h-20 hover:bg-gray ">
+            <li
+              className="items-center justify-center hidden px-4 lg:flex h-14 lg:h-20 hover:bg-gray "
+              onClick={() => setShowSearch(!showSearch)}
+            >
               <div className="text-3xl">
                 <AiOutlineSearch className="icon" />
               </div>
@@ -120,6 +131,24 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
+      {/* Search menu */}
+      <div
+        className={`relative w-full -translate-y-5   animateSearch -z-10 ${
+          showSearch ? "showSearch" : ""
+        }`}
+        ref={searchMenu}
+      >
+        <div className="container relative ">
+          <input
+            type="text"
+            placeholder="Търсене ..."
+            className="w-full py-2 pl-3 border-2 rounded-full border-primary focus:outline-none"
+          />
+          <div className="absolute text-2xl font-semibold -translate-y-1/2 cursor-pointer text-primary right-3 top-1/2 ">
+            <AiOutlineSearch />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
