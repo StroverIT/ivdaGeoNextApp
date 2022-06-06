15ad097ec0ae0b868013fd8886987e674012f176
StR0vErBG <xcreaw1@abv.bg>
2022-06-06 13:53:33 +0200

    adding route

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,5 +1,57 @@
 import React from "react";
 
+// NextJs
+import Head from "next/head";
+
+// Components
+import Input from "../components/form/Input";
+
 export default function contactUs() {
-  return <div>contactUs</div>;
+  return (
+    <>
+      <Head></Head>
+
+      <main className="mb-auto">
+        <div className="container">
+          <div>
+            <h3 className="font-medium text-xl uppercase">Контакти</h3>
+            <ul className="text-gray-[#444]">
+              <li className="py-1">
+                <span className="font-semibold text-dark pr-2">
+                  Заповядайте в нашия магазин:
+                </span>
+                <br />
+                гр.София ПК-1582 Дружба 2 РУМ Дружба 2 срещу блок 204
+              </li>
+              <li className="py-1">
+                <span className="font-semibold text-dark pr-2">
+                  Тел. / факс:
+                </span>
+                02/973 15 85
+              </li>
+              <li className="py-1">
+                <span className="font-semibold text-dark pr-1">GSM:</span> 0888
+                900746, 0879 406620
+              </li>
+              <li>
+                <span className="font-semibold text-dark pr-1">Е-mail:</span>
+                ivda_geo@abv.bg
+              </li>
+            </ul>
+          </div>
+          <div>
+            <form action="">
+              <Input
+                type="text"
+                placeholder="Име"
+                id="name"
+                isReq={true}
+                iconType="name"
+              />
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
