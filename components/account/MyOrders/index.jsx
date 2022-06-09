fc9a7c674a692fe6da8ba49a6b6d1d33a9ef527c
StR0vErBG <xcreaw1@abv.bg>
2022-06-09 18:27:36 +0200

    Chaning to component and fixing styling

--- components/account/MyOrders/index.jsx
+++ components/account/MyOrders/index.jsx
@@ -4,67 +4,85 @@ import { AiOutlineSearch } from "react-icons/ai";
 // Components
 import Pricing from "../../priceStyling/Pricing";
 
+function StatusProgress() {
+  return <td className="font-semibold text-primary">В прогрес</td>;
+}
+function StatusSent() {
+  return <td className="font-semibold text-green">Изпратена</td>;
+}
+function StatusRet() {
+  return <td className="font-semibold text-secondary">Върната</td>;
+}
+function TableRow({ id, date, total, status, isOld }) {
+  const statusDic = {
+    progress: <StatusProgress />,
+    sent: <StatusSent />,
+    returned: <StatusRet />,
+  };
+  return (
+    <tr>
+      {/* Mongodb */}
+      <td className="py-4">{id}</td>
+      <td className="hidden sm:table-cell">{date}</td>
+      <td className="hidden sm:table-cell">{total} (в лв.)</td>
+      {statusDic[status]}
+      {isOld && (
+        <td className="flex justify-center h-full items-cemter">
+          <button className="flex items-center justify-center h-full p-4 text-2xl font-semibold rounded-full cursor-pointer text-primary-lighter">
+            <AiOutlineSearch />
+          </button>
+        </td>
+      )}
+      {!isOld && (
+        <td>
+          <button className="p-2 text-sm font-semibold tracking-wide text-center text-white rounded-md cursor-pointer bg-primary-lighter">
+            ВИЖ
+          </button>
+        </td>
+      )}
+    </tr>
+  );
+}
 export default function MyOrders() {
   return (
     <section className="">
-      <h2 className="mb-2 text-3xl font-semibold text-center mt-5">
+      <h2 className="mt-5 mb-2 text-3xl font-semibold text-center">
         Направените от вас поръчки
       </h2>
       {/* If no orders */}
-      <div className="text-center text-secondary pb-10">
+      <div className="pb-10 text-center text-secondary">
         Нямате направени поръчки
       </div>
-      <table className="table-auto w-full my-table-spacing mt-10">
+      <table className="w-full mt-10 table-auto my-table-spacing">
         <thead>
-          <tr className="bg-primary text-white ">
+          <tr className="text-center text-white bg-primary">
             <th className="py-2">Поръчка</th>
-            <th>Дата</th>
-            <th>Общо</th>
+            <th className="hidden sm:table-cell">Дата</th>
+            <th className="hidden sm:table-cell">Общо</th>
             <th>Статус</th>
             <th></th>
           </tr>
         </thead>
-        <tbody className="divide-y-2 divide-gray">
-          <tr>
-            {/* Mongodb */}
-            <td className="py-4">ID на поръчката</td>
-            <td>Дата на направената поръчка</td>
-            <td>Обща сума (в лв.)</td>
-            <td className="text-primary font-semibold">В прогрес</td>
-            <td>
-              <button className="text-primary-lighter  flex justify-center items-center rounded-full p-2 text-2xl font-semibold cursor-pointer">
-                <AiOutlineSearch />
-              </button>
-            </td>
-          </tr>
-          <tr>
-            {/* Mongodb */}
-            <td className="py-4">ID на поръчката</td>
-            <td>Дата на направената поръчка</td>
-            <td>Обща сума (в лв.)</td>
-            <td className="text-green font-semibold">Изпратена</td>
-            <td>
-              <button className="bg-primary-lighter text-white text-center rounded-md p-2 text-sm font-semibold tracking-wide cursor-pointer">
-                ВИЖ
-              </button>
-            </td>
-          </tr>
-          <tr>
-            {/* Mongodb */}
-            <td className="py-4">ID на поръчката</td>
-            <td>Дата на направената поръчка</td>
-            <td>Обща сума (в лв.)</td>
-            <td className="text-secondary font-semibold">Върната</td>
-          </tr>
-          <tr>
-            {/* Mongodb */}
-            <td className="py-4">752112</td>
-            <td>12.05.1995</td>
-            <td>
-              <Pricing price={249} priceDec={22} size="xl" />
-            </td>
-            <td className="text-secondary font-semibold">Върната</td>
-          </tr>
+        <tbody className="text-center divide-y-2 divide-gray">
+          <TableRow
+            id="#12312541"
+            date="12.02.2022"
+            total={12.22}
+            status="progress"
+            isOld={true}
+          />
+          <TableRow
+            id="#12312541"
+            date="12.02.2022"
+            total={12.22}
+            status="sent"
+          />
+          <TableRow
+            id="#12312541"
+            date="12.02.2022"
+            total={12.22}
+            status="returned"
+          />
         </tbody>
       </table>
     </section>
