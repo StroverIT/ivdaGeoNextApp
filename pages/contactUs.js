6376a4a10ea39e3cf349053eb4348a1e6e0d0502
StR0vErBG <xcreaw1@abv.bg>
2022-06-15 07:52:36 +0200

    fixing styling and grid

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -10,41 +10,41 @@ import Head from "next/head";
 import Input from "../components/form/Input";
 import OutlinedBtn from "../components/buttons/Outlined";
 
-export default function contactUs() {
+export default function ContactUs() {
   return (
     <>
       <Head></Head>
 
-      <main className="my-10">
-        <div className="container lg:grid grid-cols-2 ">
+      <main className="my-10 lg:my-32">
+        <div className="container justify-between grid-cols-2 lg:grid">
           <div>
-            <h3 className="font-medium text-2xl uppercase">Контакти</h3>
-            <ul className="text-gray-250 mt-2">
+            <h3 className="text-2xl font-medium uppercase">Контакти</h3>
+            <ul className="mt-2 text-gray-250">
               <li className="py-1">
-                <span className="font-semibold text-dark pr-2">
+                <span className="pr-2 font-semibold text-dark">
                   Заповядайте в нашия магазин:
                 </span>
                 <br />
                 гр.София ПК-1582 Дружба 2 РУМ Дружба 2 срещу блок 204
               </li>
               <li className="py-1">
-                <span className="font-semibold text-dark pr-2">
+                <span className="pr-2 font-semibold text-dark">
                   Тел. / факс:
                 </span>
                 02/973 15 85
               </li>
               <li className="py-1">
-                <span className="font-semibold text-dark pr-1">GSM:</span> 0888
+                <span className="pr-1 font-semibold text-dark">GSM:</span> 0888
                 900746, 0879 406620
               </li>
               <li>
-                <span className="font-semibold text-dark pr-1">Е-mail:</span>
+                <span className="pr-1 font-semibold text-dark">Е-mail:</span>
                 ivda_geo@abv.bg
               </li>
             </ul>
           </div>
-          <div className="my-10 lg:my-0 lg:w-10/12">
-            <form action="">
+          <div className="my-10 lg:flex lg:justify-end lg:my-0">
+            <form action="" className=" lg:w-10/12">
               <Input
                 type="text"
                 placeholder="Име"
@@ -71,7 +71,7 @@ export default function contactUs() {
                   name="description"
                   id="description"
                   placeholder="Съобщение"
-                  className="w-full placeholder:text-gray-darker pl-6 border pt-2 pb-10"
+                  className="w-full pt-2 pb-10 pl-6 border placeholder:text-gray-darker"
                 ></textarea>
                 <div className="absolute top-[13px] left-1">
                   <AiFillEdit />
