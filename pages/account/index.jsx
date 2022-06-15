b436cd80678cd5bbf0ee7fc3eac807437aea2408
StR0vErBG <xcreaw1@abv.bg>
2022-06-15 13:47:24 +0200

    adding spacing and basic hover styling

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -38,8 +38,16 @@ export default function Index({ userData }) {
     setCategoryData(someData[0]);
     for (let category in categoryComp) {
       categoryComp[category][1].current.classList.remove("acc-active");
+      categoryComp[category][1].current.classList.add(
+        "hover:bg-primary-0",
+        "hover:text-primary"
+      );
     }
     someData[1].current.classList.add("acc-active");
+    someData[1].current.classList.remove(
+      "hover:bg-primary-0",
+      "hover:text-primary"
+    );
   }, [router]);
 
   return (
@@ -54,8 +62,8 @@ export default function Index({ userData }) {
             <h3 className="text-3xl pt-7">Моят акаунт</h3>
             <div className="lg:grid grid-cols-[20%80%] gap-10 my-10  relative">
               <aside className="mb-4  md:mb-0 bg-[#f5f5f5]  my-2">
-                <ul className="justify-center text-sm text-left top-28 sm:space-x-4 sm:flex lg:flex-col lg:space-x-0 lg:space-y-2 md:sticky">
-                  <li className="my-1 cursor-pointer" ref={myDetails}>
+                <ul className="justify-center text-sm text-left top-28 sm:space-x-4 sm:flex lg:flex-col lg:space-x-0 lg:space-y-2 md:sticky py-5">
+                  <li className="my-1 cursor-pointer " ref={myDetails}>
                     {/*"Icon"*/}
                     <button
                       onClick={() => changeCategory("account-details")}
@@ -65,7 +73,7 @@ export default function Index({ userData }) {
                       Моите детайли
                     </button>
                   </li>
-                  <li className="my-1 cursor-pointer" ref={myOrders}>
+                  <li className="my-1 cursor-pointer " ref={myOrders}>
                     {/*"Icon"*/}
                     <button
                       onClick={() => changeCategory("my-orders")}
@@ -75,7 +83,7 @@ export default function Index({ userData }) {
                       Направени поръчки
                     </button>
                   </li>
-                  <li className="my-1 cursor-pointer" ref={myFavourites}>
+                  <li className="my-1 cursor-pointer " ref={myFavourites}>
                     <button
                       onClick={() => changeCategory("my-favourites")}
                       type="button"
@@ -84,7 +92,7 @@ export default function Index({ userData }) {
                       Любими продукти
                     </button>
                   </li>
-                  <li className="my-1">
+                  <li className="my-1 pt-10 pb-5 sm:pt-0 sm:pb-0 lg:pt-10 ">
                     <button
                       type="button"
                       className="flex items-center justify-center h-full px-5 py-2 text-sm font-semibold text-white cursor-pointer bg-secondary"
