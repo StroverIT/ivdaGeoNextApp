e9e4e1373f10f08e9315068328d9038b052956a0
StR0vErBG <xcreaw1@abv.bg>
2022-06-16 21:36:51 +0200

    adding admin route

---
+++ pages/adminPanel/index.js
@@ -0,0 +1,39 @@
+import { getSession } from "next-auth/react";
+import React from "react";
+
+export default function Index({ userData }) {
+  return <div>index</div>;
+}
+export async function getServerSideProps(context) {
+  // Session
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/account/login",
+        permanent: false,
+      },
+    };
+  }
+  // Mongodb
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/getUser`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: session.session.user.email,
+      name: session.session.user.name,
+    }),
+  });
+  const data = await res.json();
+  if (data.role != "admin") {
+    return {
+      redirect: {
+        destination: "/account/login",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { userData: JSON.parse(JSON.stringify(data)) },
+  };
+}
