227f1cb90b55d52f343cc3a1f1b65b259d049419
StR0vErBG <xcreaw1@abv.bg>
2022-07-05 23:07:33 +0200

    Verify account rest api endpoint

--- pages/api/account/forgotten/verifyingAcc.js
+++ pages/api/account/forgotten/verifyingAcc.js
@@ -1,9 +1,63 @@
-import { MongoClient } from "mongodb";
+// Mongoose
+import { connectMongo } from "../../../../db/connectDb";
+import mongoose from "mongoose";
+// Models
+import User from "../../../../db/models/User";
+import Token from "../../../../db/models/Token";
+// Mongode
+import { ObjectId } from "mongodb";
+
+// Validators
+// import { emailVal } from "../../../../utils/validationHandler";
+
+import sendEmail from "./sendEmail";
 
 async function handler(req, res) {
   //Connect with database
   if (req.method == "POST") {
-    return res.status(201).json({ success });
+    const { email } = req.body;
+
+    await connectMongo();
+
+    const user = await User.findOne({ email });
+
+    // --- Validators
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Не е намерен такъв и-мейл", isErr: true });
+    }
+    if (user.isVerified) {
+      return res
+        .status(404)
+        .json({ message: "Вече е потвърден акаунта!", isErr: true });
+    }
+
+    // --- If everything succeed
+
+    // Create token
+    let token = await Token.findOne({ userId: ObjectId(user._id) });
+    if (!token) {
+      token = await Token.create({
+        userId: user._id,
+        token: new ObjectId(),
+      });
+    }
+    const message = `
+      <h3>За потвърждаване на имейла в ivdaGeo.bg, цъкнете линка:</h2>
+      <br>
+      <a href="https://${process.env.NEXTAUTH_URL}/api/account/verifyAccountToken/${user._id}.${token.token}">Цъкни тук</a>
+      `;
+    // Sending email
+    await sendEmail(
+      process.env.EMAIL_SEND,
+      email,
+      "Потвърждаване на и-мейла ivdaGeo",
+      message
+    );
+    return res
+      .status(201)
+      .json({ message: "Успешно изпратена заявка", isErr: false });
   }
 }
 export default handler;
