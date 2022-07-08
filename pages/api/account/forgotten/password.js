3f28916d27ad0d88a569dc61ee1bdc5a77f46e31
StR0vErBG <xcreaw1@abv.bg>
2022-07-08 11:39:37 +0200

    adding changes

--- pages/api/account/forgotten/password.js
+++ pages/api/account/forgotten/password.js
@@ -1,39 +1,59 @@
 // Mongodb
 import { connectMongo } from "../../../../db/connectDb";
+import { ObjectId } from "mongodb";
+
 import mongoose from "mongoose";
 import User from "../../../../db/models/User";
-
+import Token from "../../../../db/models/Token";
 // Validators
 import { emailVal } from "../../../../utils/validationHandler";
+
+import sendEmail from "../../sendEmail";
 
 async function handler(req, res) {
   //Connect with database
   if (req.method == "POST") {
     const errors = [];
     const { email } = req.body;
-    // Validators
+
     const emailCheck = emailVal(email);
-    if (!emailCheck.result) errors.push(emailCheck.message);
-    // Connecting to mongodb
+    if (!emailCheck.result) {
+      errors.push("Невалиден формат на и-мейл");
+    }
+    // Connecting
     await connectMongo();
-    // Checking for user
     const user = await User.findOne({ email });
-    console.log(user);
-    // If no user
     if (!user) {
-      errors.push("Няма акаунт с такъв и-мейл");
+      errors.push("Не е намерен такъв и-мейл");
     }
     // If are errors
     if (errors.length > 0) {
       return res.status(403).json(errors);
     }
     // Create Token
-
+    let token = await Token.findOne({ userId: ObjectId(user._id) });
+    if (!token) {
+      token = await Token.create({
+        userId: user._id,
+        token: new ObjectId(),
+      });
+    }
     // Add token to user
 
     // MUST SEND A PASSWORD page to change password with token URL
+    const message = `
+      <h3>За сменяне на паролата в ivdaGeo.bg.
+      </h2><a href="${process.env.NEXTAUTH_URL}/account/changePassword/${user._id}/${token.token}">Цъкнете тук</a>
+      `;
+    // Sending email
+    await sendEmail(
+      process.env.EMAIL_SEND,
+      email,
+      "Смяна на парола ivdaGeo",
+      message
+    );
     mongoose.connection.close();
-    return res.status(201).json({ message: "PBRAVO" });
+    return res.status(201);
   }
 }
 export default handler;
