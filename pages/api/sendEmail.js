8ed665f47da0050301ccb23359cdf8d3db3e922a
StR0vErBG <xcreaw1@abv.bg>
2022-07-08 11:38:02 +0200

    sendEmail api endpoint

---
+++ pages/api/sendEmail.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+const sendEmail = async (fromEmail, email, subject, text) => {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "emilzlatinov123@gmail.com",
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+    await transporter.sendMail({
+      from: fromEmail,
+      to: email,
+      subject: subject,
+      html: text,
+    });
+  } catch (error) {}
+};
+
+export default sendEmail;
