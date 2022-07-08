eaa20d010100589d936f8f539c495f38903f0e0f
StR0vErBG <xcreaw1@abv.bg>
2022-07-08 11:33:13 +0200

    adding additional feature to outlined btn

--- components/buttons/Outlined.jsx
+++ components/buttons/Outlined.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Outlined({ type, text, custom }) {
+export default function Outlined({ type, text, custom, isDisabled }) {
+  console.log(isDisabled);
   return (
     <button
       type={type}
+      disabled={isDisabled}
       className={`w-full py-2 font-medium text-white uppercase transition-colors duration-100 border px-14 bg-primary hover:bg-transparent hover:text-primary border-primary ${
         custom ? custom : ""
       }`}
